58c0e564d0b3fd86f52e7f5f4786c1df5a429550
Alexandre de Souza <alexandre@aledsz.com.br>
2016-09-18 09:39:16 -0300

    Update fixtures Update tests Clean database Remove logs

--- test/test/Fixtures.php
+++ test/test/Fixtures.php
@@ -4,12 +4,23 @@ use Library\Entities\Contact;
 
 class Fixtures
 {
-	public static function ContactData()
+	public static $contactEmail = null;
+
+	public static function newContactData()
 	{
 		$contact              = new Contact();
 		$contact->Name        = "Joseph";
 		$contact->Email       = uniqid() . "@aledsz.com.br";
 		$contact->PhoneNumber = uniqid();
+		self::$contactEmail   = $contact->Email;
+
+		return $contact;
+	}
+
+	public static function createdContactData()
+	{
+		$contact        = new Contact();
+		$contact->Email = self::$contactEmail;
 
 		return $contact;
 	}
--- test/test/TestUnitCase.php
+++ test/test/TestUnitCase.php
@@ -13,8 +13,32 @@ class TestUnitCase extends UnitTestCase
 
 	function testInsertData()
 	{
-		$contact = Fixtures::ContactData();
+		$contact = Fixtures::newContactData();
 		$contact = ContactService::create($contact);
 		$this->assertNotNull($contact);
+	}
+
+	function testFindData()
+	{
+		$contact = Fixtures::createdContactData();
+		$contact = ContactService::find($contact);
+		$this->assertNotNull($contact);
+	}
+
+	function testUpdateData()
+	{
+		$contact       = Fixtures::createdContactData();
+		$contact       = ContactService::find($contact);
+		$contact->Name = "Alexandre de Souza";
+
+		$this->assertEqual($contact->Name, "Alexandre de Souza");
+	}
+
+	function testDeleteData()
+	{
+		$contact = Fixtures::createdContactData();
+		ContactService::remove($contact);
+		$contact = ContactService::find($contact);
+		$this->assertNull($contact);
 	}
 }
