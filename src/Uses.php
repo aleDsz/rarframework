2e74c6cf570dccbc2d3709dbf0e735de4c76a456
Alexandre de Souza <alexandre@aledsz.com.br>
2020-05-20 22:29:14 -0300

    Remove old files

--- src/Uses.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/* RAR Framework */
-require_once(dirname(__FILE__) . "/RAR/RarFramework.php");
-
-?>
--- test/simpletest/extensions/coverage/coverage_writer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface CoverageWriter
-{
-    public function writeSummary($out, $variables);
-
-    public function writeByFile($out, $variables);
-}
