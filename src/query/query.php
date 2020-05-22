e052de1e7f6ce96c27d074d610fa5e9fc177ebd0
Alexandre de Souza <alexandre@aledsz.com.br>
2020-05-21 23:52:50 -0300

    Add query builder class

---
+++ src/query/query.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace rarframework\query {
+  class Query {
+    private  $schema;
+    private $where;
+    private $joins;
+    private $fields;
+    private $order_by;
+    private $group_by;
+    private $limit;
+
+    function __construct(\rarframework\Schema $schema) {
+      $this -> schema = $schema;
+    }
+
+    public function getSchema() {
+      return $this -> schema;
+    }
+  }
+}
