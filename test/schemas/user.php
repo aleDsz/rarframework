555a38e8ea1ec0c0701d93727e734adf6dfb92ef
Alexandre de Souza <alexandre@aledsz.com.br>
2020-05-23 03:22:35 -0300

    Add annotations to User schema

--- test/schemas/user.php
+++ test/schemas/user.php
@@ -2,6 +2,16 @@
 
 namespace tests\schemas {
   class User extends \rarframework\Schema {
-    public $id;
+
+    /**
+     * @primary_key id, uuid, autogenerate: true
+     * @schema users
+     * @field name, string, not_null
+     * @field email, string, default: test@test.com
+     * @field age, integer
+     */
+    function __construct() {
+      parent::__construct();
+    }
   }
 }
